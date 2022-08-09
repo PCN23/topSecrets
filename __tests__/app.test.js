4497336ff38a53b18b51af3a68b947fe34c8f345
Pedro <pedro.chn01@gmail.com>
2022-08-09 14:36:02 -0700

    adding 1st test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,49 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+//const { request } = require('express');
+const request = require('supertest');
+const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
-describe('backend-express-template routes', () => {
+
+const thisUser = {
+  email: 'user@user.com',
+  password: '1234',
+};
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? thisUser.password;
+  const agent = request.agent(app);
+  const user = await UserService.create({ ...thisUser, ...userProps });
+  const { email } = user;
+  await agent.post('/api/v1/secrets').send({ email, password });
+  return [agent, user];
+};
+
+describe('secret routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('#POST logs in a user', async () => {
+    const res = await request(app).post('/api/v1/users/sessions').send(thisUser);
+    const { email } = thisUser;
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email,
+    });
   });
+
+  it('returns the current user', async () => {
+    const [agent, thisUser] = await registerAndLogin();
+    const me = await agent.get('/api/v1/users/sessions');
+
+    expect(me.body).toEqual({
+      ...thisUser,
+      exp: expect.any(Number),
+      iat: expect.any(Number),
+    });
+  });
+
   afterAll(() => {
     pool.end();
   });
